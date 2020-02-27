4f9c1f00fb99711686f11d54246cba8e4f72a389
ryota <sigurebidesu@yahoo.co.jp>
2020-02-27 22:05:19 +0900

    webpacker environment.js remove vue

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,6 +1,4 @@
 const { environment } = require('@rails/webpacker')
-const { VueLoaderPlugin } = require('vue-loader')
-const vue = require('./loaders/vue')
 
 const webpack = require('webpack');
 environment.plugins.prepend('Provide',
