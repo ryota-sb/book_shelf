5be5ea35a709599bdb7715cd2f6346aaa9fe717e
ryota <sigurebidesu@yahoo.co.jp>
2020-02-28 11:08:38 +0900

    users controller fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      user.send_activation_email
+      @user.send_activation_email
       flash[:info] = 'メールを送信しました。メールを確認して登録を完了させよう！'
       redirect_to root_url
     else
