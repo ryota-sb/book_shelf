e0b0eb93e2c95b3b4e23f1309d6b0b533d9f9fdb
ryota <sigurebidesu@yahoo.co.jp>
2020-03-01 15:49:36 +0900

    mailer config

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      @user.send_activation_email
+      account_activation(@user).deliver_now
       flash[:info] = 'メールを送信しました。メールを確認して登録を完了させよう！'
       redirect_to root_url
     else
--- app/models/user.rb
+++ app/models/user.rb
@@ -49,10 +49,6 @@ class User < ApplicationRecord
     BCrypt::Password.new(digest).is_password?(token)
   end
 
-  def send_activation_email
-    UserMailer.account_activation(self).deliver_now
-  end
-
   def activation
     update_columns(activated: true, activated_at: Time.zone.now)
   end
