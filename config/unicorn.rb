322067362d60408f169c59895bec619616a91008
ryota <sigurebidesu@yahoo.co.jp>
2020-03-13 21:38:57 +0900

    image サイズ制限

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,43 +1,31 @@
-app_path = File.expand_path('../../', __FILE__)
+$worker  = 2
+$timeout = 30
+$app_dir = "/var/www/rails/book_shelf"
+$listen  = File.expand_path 'tmp/unicorn.sock', $app_dir
+$pid     = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
+$std_log = File.expand_path 'log/unicorn.log', $app_dir
 
-worker_processes 1
-
-working_directory app_path
-
-pid "#{app_path}/tmp/pids/unicorn.pid"
-
-listen 3000
-
-stderr_path "#{app_path}/log/unicorn.stderr.log"
-
-timeout 60
+worker_processes  $worker
+working_directory $app_dir
+stderr_path       $std_log
+stdout_path       $std_log
+timeout           $timeout
+listen            $listen
+pid               $pid
 
 preload_app true
-GC.respond_to?(:copy_on_write_friendly=) && GC.copy_on_write_friendly = true
-
-check_client_connection false
-
-run_once = true
 
 before_fork do |server, worker|
-  defined?(ActiveRecord::Base) &&
-    ActiveRecord::Base.connection.disconnect!
-
-  if run_once
-    run_once = false
-  end
-
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
   old_pid = "#{server.config[:pid]}.oldbin"
-  if File.exist?(old_pid) && server.pid != old_pid
+  if old_pid != server.pid
     begin
-      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
-      Process.kill(sig, File.read(old_pid).to_i)
-    rescue Errno::ENOENT, Errno::ESRCH =>  e
-      logger.error e
+      Process.kill "QUIT", File.read(old_pid).to_i
+    rescue Errno::ENOENT, Errno::ESRCH
     end
   end
 end
 
 after_fork do |server, worker|
-  defined?(ActiveRecord::Base) && ActiveRecord::Base.establish_connection
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
 end
